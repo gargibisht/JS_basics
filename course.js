3488d08d2c654bfdcc371590270b27837e01b5c3
gargibisht <bisht.gargi19@gmail.com>
2021-09-16 17:44:40 +0530

    Course_ready

---
+++ course.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+    $("button").click(function() {
+        $("body").css({
+            "font-family": "'Kaisei HarunoUmi', serif",
+            "background-image": "linear-gradient(43deg, #C850C0, #FFCC70)"
+        });
+        $(".head_para").css({
+            "text-align": "center",
+            "font-size": "28px",
+            "margin": "20px",
+            "color": "white",
+            "text-shadow": "0 0 3px #FF0000, 0 0 5px #0000FF"
+
+        });
+
+        $(".parab").css({
+            "margin": "20px",
+            "font-size": "18px"
+        });
+
+        $("#head1").css({
+            "margin-bottom": "20px",
+            "font-weight": "600",
+            "color": "white",
+            "text-shadow": "0 0 3px #FF0000, 0 0 5px #0000FF"
+        });
+
+        $(".t1,th,td").css({
+            "border": "1px solid black",
+            "margin-top": "20px"
+        });
+
+        $(".t2,td,th").css({
+            "border": "1px solid black",
+            "margin-top": "20px"
+
+        });
+
+        $(".nxtpara").css({
+            "margin": "20px",
+            "font-size": "18px"
+        });
+
+        $("#head2").css({
+            "margin-bottom": "20px",
+            "font-weight": "600",
+            "color": "white",
+            "text-shadow": "0 0 3px #FF0000, 0 0 5px #0000FF"
+        });
+
+        $(".nxtpara1").css({
+            "margin": "20px",
+            "font-size": "18px",
+
+        });
+
+        $("#head3").css({
+            "margin-bottom": "20px",
+            "font-weight": "600",
+            "color": "white",
+            "text-shadow": "0 0 3px #FF0000, 0 0 5px #0000FF"
+        });
+    });
+});
